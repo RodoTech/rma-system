083e1d47b08152fabaae79729e56fffa04bbda49
picachu <eduardo.alej.aguirre@gmail.com>
2013-08-31 18:33:37 -0300

    Reparaciones de Pedidos
    Permite agregar y listar las Reparaciones de los peddidos
    Reparaicon yReparacionesRepo

--- dom/src/main/java/dom/rma/Reparacion.java
+++ dom/src/main/java/dom/rma/Reparacion.java
@@ -4,14 +4,30 @@
  */
 package dom.rma;
 
+import javax.jdo.annotations.DatastoreIdentity;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.Version;
+import javax.jdo.annotations.VersionStrategy;
 import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.AutoComplete;
+import org.apache.isis.applib.annotation.MemberGroups;
+import org.apache.isis.applib.annotation.ObjectType;
+import org.apache.isis.applib.value.Money;
 import org.joda.time.LocalDate;
 
 /**
  *
  * @author Malgav5
  */
+@PersistenceCapable(identityType=IdentityType.DATASTORE)
+@DatastoreIdentity(strategy=IdGeneratorStrategy.IDENTITY)
+@Version(strategy=VersionStrategy.VERSION_NUMBER, column="VERSION")
+@ObjectType("Reparacion")
+@AutoComplete(repository=ReparacionesRepo.class, action="autoComplete")
+@MemberGroups({"Datos Reparaciones"})
 public class Reparacion {
   
     @Persistent
@@ -22,6 +38,46 @@ public class Reparacion {
     private LocalDate fechaReparacion;
     @Persistent
     private String observaciones;
+    @Persistent
+    private Boolean terminado;
+    @Persistent
+    private Boolean reparacionExistosa;
+    @Persistent
+    private String detalleReparacion;
+    @Persistent 
+    private Money montoReparacion;
+
+    public Boolean getTerminado() {
+        return terminado;
+    }
+
+    public void setTerminado(Boolean terminado) {
+        this.terminado = terminado;
+    }
+
+    public Boolean getReparacionExistosa() {
+        return reparacionExistosa;
+    }
+
+    public void setReparacionExistosa(Boolean reparacionExistosa) {
+        this.reparacionExistosa = reparacionExistosa;
+    }
+
+    public String getDetalleReparacion() {
+        return detalleReparacion;
+    }
+
+    public void setDetalleReparacion(String detalleReparacion) {
+        this.detalleReparacion = detalleReparacion;
+    }
+
+    public Money getMontoReparacion() {
+        return montoReparacion;
+    }
+
+    public void setMontoReparacion(Money montoReparacion) {
+        this.montoReparacion = montoReparacion;
+    }
 
     public Pedido getPedido() {
         return pedido;
--- dom/src/main/java/dom/rma/ReparacionesRepo.java
+++ dom/src/main/java/dom/rma/ReparacionesRepo.java
@@ -4,12 +4,58 @@
  */
 package dom.rma;
 
+import java.util.List;
+
 import org.apache.isis.applib.AbstractFactoryAndRepository;
+import org.apache.isis.applib.annotation.ActionSemantics;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.value.Money;
+import org.joda.time.LocalDate;
 
 /**
  *
  * @author Malgav5
  */
 public class ReparacionesRepo extends  AbstractFactoryAndRepository {
-    
+     @MemberOrder(sequence = "1")
+    public Reparacion newRecepcion(
+            @Named("Pedido") Pedido pedido, 
+            @Named("Fecha Ingreso REcepcion") LocalDate fechaIngreso,
+            @Named("Fecha Ingreso fechaReparacion") LocalDate fechaReparacion,
+            @Named("Paquete String") String observaciones,
+            @Named("terminado") Boolean terminado,
+            @Named("reparacionExistosa") Boolean reparacionExistosa,
+            @Named("Paquete detalleReparacion") String detalleReparacion,
+            @Named("Paquete montoReparacion") Money montoReparacion) {
+        return addReparacion(pedido,fechaIngreso,fechaReparacion,observaciones,terminado,reparacionExistosa,detalleReparacion,montoReparacion);   
+
+    }
+
+    @ActionSemantics(ActionSemantics.Of.SAFE)
+    @MemberOrder(sequence = "4")
+    public List<Reparacion> allReparaciones() {
+    	
+    	 final List<Reparacion> reparaciones;
+        reparaciones = allInstances(Reparacion.class);
+		 return reparaciones; 
+       
+    }
+    @Hidden
+    private Reparacion addReparacion(Pedido pedido, LocalDate fechaIngreso, LocalDate fechaReparacion, String observaciones, Boolean terminado, Boolean reparacionExistosa, String detalleReparacion, Money montoReparacion) {
+  
+        final Reparacion reparacion = newTransientInstance(Reparacion.class);
+            reparacion.setPedido(pedido);
+            reparacion.setFechaIngreso(fechaIngreso);
+            reparacion.setFechaReparacion(fechaReparacion);
+            reparacion.setObservaciones(observaciones);
+            reparacion.setTerminado(terminado);
+            reparacion.setReparacionExistosa(reparacionExistosa);
+            reparacion.setDetalleReparacion(detalleReparacion);
+            reparacion.setMontoReparacion(montoReparacion);
+            persist(reparacion);
+        return reparacion;
+    }
+
 }
